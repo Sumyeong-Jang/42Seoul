41adf4afeb6748e53bd278b32843f7955726c5f3
Sumyeong Jang <sumjang@c6r7s6.42seoul.kr>
2022-02-05 23:18:17 +0900

    Update 2022-02-05

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: sumjang <sumjang@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 11:58:55 by sumjang           #+#    #+#             */
-/*   Updated: 2021/11/22 16:35:58 by sumjang          ###   ########.fr       */
+/*   Updated: 2022/02/05 23:13:59 by sumjang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@ void	*ft_memset(void *b, int c, size_t len)
 		return (NULL);
 	dest = b;
 	src = c;
-	len = 0;
+	i = 0;
 	while (i < len)
 	{
-		*dest[i] = src;
+		dest[i] = src;
 		i++;
 	}
 	return (b);
