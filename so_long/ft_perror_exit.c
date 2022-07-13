ba481a649a9a7e347cda752d29d097b8493237a8
Sumyeong Jang <sumjang@c8r8s5.42seoul.kr>
2022-07-13 17:25:07 +0900

    delete

--- so_long/ft_perror_exit.c
+++
@@ -1,7 +0,0 @@
-#include "../include/libft.h"
-
-void	ft_perror_exit(char *s)
-{
-	perror(s);
-	exit(EXIT_FAILURE);
-}
--- so_long/ft_pstr_exit.c
+++
@@ -1,7 +0,0 @@
-#include "../include/libft.h"
-
-void	ft_pstr_exit(char *s)
-{
-	ft_putendl_fd(s, STDERR_FILENO);
-	exit(EXIT_FAILURE);
-}
